c62df19970768f3fe6984389fdea2c238f685529
Michael Williams <michaelm.w@outlook.com>
2013-07-04 13:04:35 -0400

    Added read from index.html

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var infile = "index.html";
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
+
+var buf = new Buffer(fs.readFileSync(infile));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
