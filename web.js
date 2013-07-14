8dc7136eafd65c2ed87e5f1770f9791f4e3af4ce
Michael Williams <michaelm.w@outlook.com>
2013-07-14 01:16:43 -0400

    HW4 changed port number

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
 
 var buf = new Buffer(fs.readFileSync(infile));
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
