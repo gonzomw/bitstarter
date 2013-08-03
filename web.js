375cdc67e8badc02a67598e2883e871197ca9579
Michael Williams <michaelm.w@outlook.com>
2013-08-03 19:04:06 -0400

    add contact page

--- web.js
+++ web.js
@@ -3,11 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var infile = "index.html";
+var contactfname = "contact.html";
 app.get('/', function(request, response) {
   response.send(buf.toString());
 });
+app.get('/contact/', function(request, response) {
+  response.send(contactBuf.toString());
+});
 
 var buf = new Buffer(fs.readFileSync(infile));
+var contactBuf = new Buffer(fs.readFileSync(contactfname));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
